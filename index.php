615f108c7174b54f934043ba5876f6929971379d
Satuk Buğrahan ÖZTÜRK <sbozturk1907@gmail.com>
2017-07-18 14:13:34 +0300

    added divisions

--- index.php
+++ index.php
@@ -1,160 +1,51 @@
 <!DOCTYPE html>
-<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<html class="no-js" lang="en">
 <head>
-
-   <!--- Basic Page Needs
-   ================================================== -->
-   <meta charset="utf-8">
-	<title>Keep It Simple.</title>
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- mobile specific metas
-   ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-   <!-- CSS
-    ================================================== -->
-   <link rel="stylesheet" href="css/default.css">
-	<link rel="stylesheet" href="css/layout.css">
-	<link rel="stylesheet" href="css/media-queries.css">
-
-   <!-- Script
-   ================================================== -->
-	<script src="js/modernizr.js"></script>
-
-   <!-- Favicons
-	================================================== -->
-	<link rel="shortcut icon" href="favicon.png" >
-
+   <?php require_once "head.php"; ?>
 </head>
-
 <body>
-
 <?php require_once "header.php"; ?>
-
    <!-- Content
    ================================================== -->
    <div id="content-wrap">
 
    	<div class="row">
 
-   		<div id="main" class="eight columns">
+      <div id="main" class="eight columns">
+          <?php
+          $query = $db -> query("SELECT * FROM b_gonderiler ORDER BY tarih DESC", PDO::FETCH_ASSOC);
+          if ($query->rowCount()) {
+            foreach ($query as $row) { ?>
 
-	   		<article class="entry">
+              <article class="entry">
 
-					<header class="entry-header">
+                <header class="entry-header">
 
-						<h2 class="entry-title">
-							<a href="single.html" title="">Hey, We Love Open Sans!</a>
-						</h2>
+                  <h2 class="entry-title">
+                    <a href="single.php" title=""><?php echo $row["baslik"];?></a>
+                  </h2>
 
-						<div class="entry-meta">
-							<ul>
-								<li>July 12, 2014</li>
-								<span class="meta-sep">&bull;</span>
-								<li><a href="#" title="" rel="category tag">Ghost</a></li>
-								<span class="meta-sep">&bull;</span>
-								<li>John Doe</li>
-							</ul>
-						</div>
+                  <div class="entry-meta">
+                    <ul>
+                      <li><?php echo $row["tarih"]; ?></li>
+                      <span class="meta-sep">&bull;</span>
+                      <li><a href="#" title="" rel="category tag">Ghost</a></li>
+                      <span class="meta-sep">&bull;</span>
+                      <li>John Doe</li>
+                    </ul>
+                  </div>
 
-					</header>
+                </header>
 
-					<div class="entry-content">
-						<p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.</p>
-					</div>
+                <div class="entry-content">
+                  <p><?php echo $row["icerik"]; ?></p>
+                </div>
 
-				</article> <!-- end entry -->
-
-				<article class="entry">
-
-					<header class="entry-header">
-
-						<h2 class="entry-title">
-							<a href="single.html" title="">We All Love Good Typography.</a>
-						</h2>
-
-						<div class="entry-meta">
-							<ul>
-								<li>July 11, 2014</li>
-								<span class="meta-sep">&bull;</span>
-								<li>
-									<a href="#" title="" rel="category tag">Wordpress</a>,
-									<a href="#" title="" rel="category tag">Ghost</a>
-								</li>
-								<span class="meta-sep">&bull;</span>
-								<li>Jane Doe</li>
-							</ul>
-						</div>
-
-					</header>
-
-					<div class="entry-content">
-						<p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in.</p>
-					</div>
-
-				</article> <!-- end entry -->
-
-				<article class="entry">
-
-					<header class="entry-header">
-
-						<h2 class="entry-title">
-							<a href="single.html" title="">White Space Everywhere.</a>
-						</h2>
-
-						<div class="entry-meta">
-							<ul>
-								<li>July 10, 2014</li>
-								<span class="meta-sep">&bull;</span>
-								<li>
-									<a href="#" title="" rel="category tag">Joomla</a>,
-									<a href="#" title="" rel="category tag">Drupal</a>
-								</li>
-								<span class="meta-sep">&bull;</span>
-								<li>Naruto Uzumaki</li>
-							</ul>
-						</div>
-
-					</header>
-
-					<div class="entry-content">
-						<p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.</p>
-					</div>
-
-				</article> <!-- end entry -->
-
-				<article class="entry">
-
-					<header class="entry-header">
-
-						<h2 class="entry-title">
-							<a href="single.html" title="">Simple And Minimalist Designs.</a>
-						</h2>
-
-						<div class="entry-meta">
-							<ul>
-								<li>July 09, 2014</li>
-								<span class="meta-sep">&bull;</span>
-								<li>
-									<a href="#" title="" rel="category tag">Ghost</a>
-								</li>
-								<span class="meta-sep">&bull;</span>
-								<li>Naruto Uzumaki</li>
-							</ul>
-						</div>
-
-					</header>
-
-					<div class="entry-content">
-						<p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.</p>
-					</div>
-
-				</article> <!-- end entry -->
+              </article> <!-- end entry -->
+<?php
+            }
+          }
+          ?>
 
 
    		</div> <!-- end main -->
