afe41c6cbeb518083a03a30c9849a21bcf67e859
Satuk Buğrahan ÖZTÜRK <sbozturk1907@gmail.com>
2017-07-19 15:53:44 +0300

    echo deleted

--- database.php
+++ database.php
@@ -5,7 +5,6 @@ $user = "root";
 $pass = "";
 try {
   $db = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$user,$pass);
-  echo "Veritabanı bağlantı işlemi başarılı";
 } catch (PDOException $e) {
     print $e -> getMessage();
 }
